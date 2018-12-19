e5b98457e809b4ea603c02fccd116a2857abf53c
SeoJangHyuck <sjh938606@gmail.com>
2018-12-19 19:38:46 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $(function(){
+    // Header Event
+    $(window).scroll(function(){
+        var sct = $(window).scrollTop();
+        if(sct>0){
+            $("#l_hd, .l_mlogo>a").addClass("l_hd_on");
+        }else{
+            $("#l_hd, .l_mlogo>a").removeClass("l_hd_on");
+        }
+    });
 
+    // Search Event
+    $(".l_s_open").click(function(){
+        $(".l_search").stop().fadeIn(500);
+    });
+    $(".l_s_close").click(function(){
+        $(".l_search").stop().fadeOut(500);
+    });
 }); // End
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.l_mb_slider').bxSlider({
+        mode: "fade",
+        pager: true,
+        auto: true,
+        autoControls: true,
+        pause: 3000
+    });
+}); // End
