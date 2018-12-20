9dd9425ec90bfee64962be28d4f1776aa7074dfc
SeoJangHyuck <sjh938606@gmail.com>
2018-12-20 18:16:26 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(function(){
-    $('.l_mb_slider').bxSlider({
+    $(".l_mb_slider").bxSlider({
         mode: "fade",
-        pager: true,
         auto: true,
         autoControls: true,
         pause: 3000
+    });
+    $(".l_news_slider").bxSlider({
+        pager: false,
+        controls:false,
+        pause: 3000,
+        onSlideAfter: function($slideElement, oldIndex, newIndex){
+            $(".l_news_slider>li").removeClass("l_news_on");
+            $slideElement.addClass("l_news_on");
+        },
+        onSliderLoad: function(currentIndex){
+            $(".l_news_slider>li").eq(currentIndex+1).addClass("l_news_on");
+        }
     });
 }); // End
